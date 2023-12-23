75bedb9e08bc51043e403b6e6503cabefe3093c8
Nicholas <nick.nogue28@outlook.com>
2023-12-23 19:07:55 -0300

    remove unnecessary struct

--- search-api/cmd/main.go
+++ search-api/cmd/main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"context"
-
 	"github.com/joho/godotenv"
 	"github.com/nichol20/rhythmicity/search-api/application/grpc"
-	"github.com/nichol20/rhythmicity/search-api/application/grpc/pb"
 	"github.com/nichol20/rhythmicity/search-api/infrastructure/db"
 )
-
-type SearchServer struct {
-	pb.UnimplementedSearchServer
-}
-
-func (s SearchServer) Search(context.Context, *pb.SearchRequest) (*pb.SearchResponse, error) {
-	return nil, nil
-}
 
 func main() {
 	godotenv.Load()
