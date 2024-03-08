83393ea004bb19d63696c43f3d03038c1d1c9382
Nicholas <nick.nogue28@outlook.com>
2024-03-07 23:22:41 -0300

    fix typo and sintax error

--- data-api/internal/db/sql/album_query.sql
+++ data-api/internal/db/sql/album_query.sql
@@ -8,10 +8,10 @@ WHERE a.id = $1;
 SELECT url, width, height FROM album_images_spotify WHERE spotifyId = $1;
 
 -- name: GetAlbumGenres :many
-SELECT genre FROM albums_genres ag INNER JOIN genres g ON ag.genreId = g.id WHERE ag.id = $1;
+SELECT genre FROM albums_genres ag INNER JOIN genres g ON ag.genreId = g.id WHERE ag.albumId = $1;
 
 -- name: GetAlbumStyles :many
-SELECT style FROM albums_styles ast INNER JOIN styles s ON ast.styleId = s.id WHERE ast.id = $1;
+SELECT style FROM albums_styles ast INNER JOIN styles s ON ast.styleId = s.id WHERE ast.albumId = $1;
 
 -- name: GetAlbumByTrackId :one
 SELECT a.id albumId, a.name, a.totalTracks, a.spotifyId, sp.popularity spotifyPopularity, sp.releaseDate spotifyReleaseDate
