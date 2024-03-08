991011fc9a276b7f5d4c84f8397725e5235b516a
Nicholas <nick.nogue28@outlook.com>
2024-03-07 23:18:11 -0300

    update queries

--- data-api/internal/db/sql/artist_query.sql
+++ data-api/internal/db/sql/artist_query.sql
@@ -1,29 +1,31 @@
 -- name: GetArtist :one
 SELECT 
-	a.id artistId, a.name, a.spotifyid, sp.popularity
+	a.id artistId, a.name, a.spotifyId, sp.popularity spotifyPopularity
 FROM artists a
 INNER JOIN artist_data_spotify sp ON a.spotifyId = sp.id
 WHERE a.id = $1;
 
 -- name: GetArtistSpotifyImages :many
-SELECT * FROM artist_images_spotify WHERE spotifyId = $1
+SELECT url, width, height FROM artist_images_spotify WHERE spotifyId = $1
 
 -- name: GetArtistGenres :many
-SELECT genre 
-FROM artists_genres ag
-INNER JOIN genres g ON ag.genreId = g.id
-WHERE ag.artistId = $1;
+SELECT genre FROM artists_genres ag INNER JOIN genres g ON ag.genreId = g.id WHERE ag.artistId = $1;
 
 -- name: GetArtistStyles :many
-SELECT style 
-FROM artists_styles ast
-INNER JOIN styles s ON ast.styleId = s.id
-WHERE ast.artistId = $1;
+SELECT style FROM artists_styles ast INNER JOIN styles s ON ast.styleId = s.id WHERE ast.artistId = $1;
 
 -- name: GetArtistsByTrackId :many
 SELECT 
-	a.id artistId, a.name, a.spotifyid, sp.popularity
+	a.id artistId, a.name, a.spotifyId, sp.popularity spotifyPopularity
 FROM artists_tracks at
 INNER JOIN artists a ON a.id = at.artistId
 INNER JOIN artist_data_spotify sp ON a.spotifyId = sp.id
 WHERE at.trackId = $1;
+
+-- name: GetArtistsByAlbumId :many
+SELECT 
+	a.id artistId, a.name, a.spotifyId, sp.popularity spotifyPopularity
+FROM artists_albums aral
+INNER JOIN artists a ON aral.artistId = a.id
+INNER JOIN artist_data_spotify sp ON a.spotifyId = sp.id
+WHERE aral.albumId = $1;
--- data-api/internal/db/sql/track_query.sql
+++ data-api/internal/db/sql/track_query.sql
@@ -32,16 +32,10 @@ WHERE t.id = $1;
 SELECT * FROM track_thumbnails_youtube WHERE youtubeDataId = $1;
 
 -- name: GetTrackGenres :many
-SELECT genre 
-FROM tracks_genres tg
-INNER JOIN genres g ON tg.genreId = g.id
-WHERE tg.trackId = $1;
+SELECT genre FROM tracks_genres tg INNER JOIN genres g ON tg.genreId = g.id WHERE tg.trackId = $1;
 
 -- name: GetTrackStyles :many
-SELECT style 
-FROM tracks_styles ts
-INNER JOIN styles s ON ts.styleId = s.id
-WHERE ts.trackId = $1;
+SELECT style FROM tracks_styles ts INNER JOIN styles s ON ts.styleId = s.id WHERE ts.trackId = $1;
 
 -- name: GetTrackArtistIds :many
 SELECT artistId FROM artists_tracks WHERE trackId = $1;
@@ -59,3 +53,16 @@ INNER JOIN track_data_spotify sp ON t.spotifyId = sp.id
 INNER JOIN track_data_youtube y ON t.youtubeDataId = y.id
 INNER JOIN track_statistics_youtube sy ON y.id = sy.youtubeDataId
 WHERE at.artistId = $1;
+
+-- name: GetTracksByAlbumId :many
+SELECT 
+	t.id trackId, t.albumId, t.explicit, t.playCount, t.spotifyId, t.lyrics,
+	sp.title spotifyTitle, sp.popularity spotifyPopularity, sp.durationMs spotifyDurationMs,
+    y.youtubeId, y.title youtubeTitle, y.durationMs youtubeDurationMs, y.publishedAt youtubePublishedAt,
+    sy.viewCount youtubeViewCount, sy.likeCount youtubeLikeCount, sy.favoriteCount youtubeFavoriteCount, 
+    sy.commentCount youtubeCommentCount
+FROM tracks t
+INNER JOIN track_data_spotify sp ON t.spotifyId = sp.id
+INNER JOIN track_data_youtube y ON t.youtubeDataId = y.id
+INNER JOIN track_statistics_youtube sy ON y.id = sy.youtubeDataId
+WHERE t.albumId = $1;
