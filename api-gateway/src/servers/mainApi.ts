c6360da31e11c5a3384823f21f3d770b7c0cff09
Nicholas <nick.nogue28@outlook.com>
2024-03-16 16:23:38 -0300

    add main api

---
+++ api-gateway/src/servers/mainApi.ts
@@ -0,0 +1,33 @@
+import path from 'path'
+import protoLoader from "@grpc/proto-loader"
+import grpcLibrary from "@grpc/grpc-js"
+import { ProtoGrpcType as AlbumProtoGrpcType } from '../proto/album'
+import { ProtoGrpcType as TrackProtoGrpcType } from '../proto/track'
+import { ProtoGrpcType as ArtistProtoGrpcType } from '../proto/artist'
+
+type ProtoGrpcType = AlbumProtoGrpcType & TrackProtoGrpcType & ArtistProtoGrpcType
+
+const protoDir = path.resolve(__dirname, '../proto')
+const serverUrl = process.env.MAIN_API_URL || "http://localhost:50051"
+
+const protoFiles = [
+  `${protoDir}/album.proto`,
+  `${protoDir}/artist.proto`,
+  `${protoDir}/track.proto`,
+  `${protoDir}/common.proto`,
+]
+
+const protoLoaderOptions: protoLoader.Options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+}
+
+const packageDefinition = protoLoader.loadSync(protoFiles, protoLoaderOptions)
+const mainApiProto = ((grpcLibrary.loadPackageDefinition(packageDefinition) as unknown) as ProtoGrpcType).rhythmicity.main_api
+
+export const albumClient = new mainApiProto.Album(serverUrl, grpcLibrary.credentials.createInsecure())
+export const trackClient = new mainApiProto.Track(serverUrl, grpcLibrary.credentials.createInsecure())
+export const artistClient = new mainApiProto.Artist(serverUrl, grpcLibrary.credentials.createInsecure())
