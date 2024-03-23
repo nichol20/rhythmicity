ef8fda399fff80770b8ff393c36cf85110fdccc2
Nicholas <nick.nogue28@outlook.com>
2024-03-22 23:13:08 -0300

    add images

---
+++ rhythmicity-app/src/assets/index.ts
@@ -0,0 +1,3 @@
+export { default as homeBackground } from './images/home-background.png'
+export { default as collectionBackground } from './images/collection-background.jpeg'
+export { default as logo } from './images/logo.png'
