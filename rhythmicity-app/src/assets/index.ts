1e41054fa6e4b47bd3eea9ee2f40d51a31621433
Nicholas <nick.nogue28@outlook.com>
2024-03-31 19:15:45 -0300

    add watch icon

--- rhythmicity-app/src/assets/index.ts
+++ rhythmicity-app/src/assets/index.ts
@@ -1,8 +1,9 @@
-export { default as homeBackground } from './images/home-background.png'
-export { default as collectionBackground } from './images/collection-background.jpeg'
-export { default as logo } from './images/logo.png'
+export { default as homeBackground } from "./images/home-background.png";
+export { default as collectionBackground } from "./images/collection-background.jpeg";
+export { default as logo } from "./images/logo.png";
 
-export { default as playIcon } from './images/play.svg'
-export { default as searchIcon } from './images/search.svg'
-export { default as closeIcon } from './images/close.svg' 
-export { default as menuIcon } from './images/menu.svg'
+export { default as playIcon } from "./images/play.svg";
+export { default as searchIcon } from "./images/search.svg";
+export { default as closeIcon } from "./images/close.svg";
+export { default as menuIcon } from "./images/menu.svg";
+export { default as watchIcon } from "./images/watch.svg";
