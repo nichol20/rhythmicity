b937c28b87fbbd6cb81992c53de098e03933c7ba
Nicholas <nick.nogue28@outlook.com>
2024-04-01 22:53:17 -0300

    add kind to Card

--- rhythmicity-app/src/components/Card/index.tsx
+++ rhythmicity-app/src/components/Card/index.tsx
@@ -9,30 +9,31 @@ interface CardProps {
     title: string
     description: string
     isArtist?: boolean
-    isPlayable?: boolean 
+    isPlayable?: boolean
+    kind?: 'normal' | 'big'
 }
 
-const Card = ({ image, title, description, isArtist=false, isPlayable=false }: CardProps) => {
-  return (
-    <div className={styles.card}>
-        <div className={styles.container}>
-            <div className={styles.imageBox} data-card-type={isArtist ? "artist": ""}>
-                <div className={styles.boxContent}>
-                    <Image src={image} alt={title}/>
+const Card = ({ image, title, description, isArtist = false, isPlayable = false, kind = "normal" }: CardProps) => {
+    return (
+        <div className={styles.card} data-kind={kind}>
+            <div className={styles.container}>
+                <div className={styles.imageBox} data-card-type={isArtist ? "artist" : ""}>
+                    <div className={styles.boxContent}>
+                        <Image src={image} alt={title} />
+                    </div>
+                </div>
+                <div className={styles.details}>
+                    <h3 className={styles.title}>{title}</h3>
+                    <p className={styles.description}>{description}</p>
                 </div>
             </div>
-            <div className={styles.details}>
-                <h3 className={styles.title}>{title}</h3>
-                <p className={styles.description}>{description}</p>
-            </div>
+            {isPlayable && (
+                <button className={styles.playBtn}>
+                    <Image src={playIcon} alt="play" />
+                </button>
+            )}
         </div>
-        {isPlayable && (
-            <button className={styles.playBtn}>
-                <Image src={playIcon} alt="play" />
-            </button>
-        )}
-    </div>
-  );
+    );
 };
 
 export default Card;
