2677d9bd59655e9fca454553ed6376cbe93c7d73
Nicholas <nick.nogue28@outlook.com>
2024-04-02 22:37:42 -0300

    add more icons

--- rhythmicity-app/src/assets/index.ts
+++ rhythmicity-app/src/assets/index.ts
@@ -7,3 +7,7 @@ export { default as searchIcon } from "./images/search.svg";
 export { default as closeIcon } from "./images/close.svg";
 export { default as menuIcon } from "./images/menu.svg";
 export { default as watchIcon } from "./images/watch.svg";
+export { default as nextIcon } from "./images/next.svg";
+export { default as randomIcon } from "./images/random.svg";
+export { default as repeatIcon } from "./images/repeat.svg";
+export { default as repeatOneIcon } from "./images/repeat-one.svg";
