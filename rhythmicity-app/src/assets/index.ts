ef6ae1946e09f3980111875a5c5e8e29e9f4b105
Nicholas <nick.nogue28@outlook.com>
2024-04-07 23:14:43 -0300

    update icons

--- rhythmicity-app/src/assets/index.ts
+++ rhythmicity-app/src/assets/index.ts
@@ -2,13 +2,21 @@ export { default as homeBackground } from "./images/home-background.png";
 export { default as collectionBackground } from "./images/collection-background.jpeg";
 export { default as logo } from "./images/logo.png";
 
-export { default as playIcon } from "./images/play.svg";
 export { default as searchIcon } from "./images/search.svg";
 export { default as closeIcon } from "./images/close.svg";
 export { default as menuIcon } from "./images/menu.svg";
 export { default as watchIcon } from "./images/watch.svg";
-export { default as nextIcon } from "./images/next.png";
-export { default as randomIcon } from "./images/random.png";
+
+// Player controls
+export { default as playIcon } from "./images/play.svg";
+export { default as nextIcon } from "./images/next.svg";
+export { default as shuffleIcon } from "./images/shuffle.svg";
 export { default as repeatIcon } from "./images/repeat.png";
 export { default as repeatOneIcon } from "./images/repeat-one.png";
-export { default as pauseIcon } from "./images/pause.png";
+export { default as pauseIcon } from "./images/pause.svg";
+
+// Player options
+export { default as volumeIcon } from "./images/volume.svg";
+export { default as volumeDownIcon } from "./images/volume-down.svg";
+export { default as volumeOffIcon } from "./images/volume-off.svg";
+export { default as lyricsIcon } from "./images/lyrics.png";
