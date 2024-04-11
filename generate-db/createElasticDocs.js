6649b4d29234be9cdd8b855959ef3b3f14819bff
Nicholas <nick.nogue28@outlook.com>
2024-04-11 00:01:40 -0300

    add artist and album documents

--- generate-db/createElasticDocs.js
+++ generate-db/createElasticDocs.js
@@ -2,23 +2,66 @@ const fs = require("node:fs/promises")
 const tracks = require('./data/app/tracks.json')
 const artists = require('./data/app/artists.json')
 const albums = require('./data/app/albums.json')
+const docsPath = './data/elastic'
 
 const createTrackDocuments = async () => {
     const trackDocs = tracks.map(track => ({
             id: track.id,
-            trackName: track.spotify.title,
+            name: track.spotify.title,
             artistNames: track.artistIds.map(id => artists.find(art => art.id === id).name),
             albumName: albums.find(alb => alb.id === track.albumId).name,
             explicit: track.explicit,
             playCount: track.playCount,
+            popularity: track.spotify.popularity,
+            durationMs: track.youtube.durationMs,
             genres: track.genres,
             styles: track.styles,
-            imageUrl: track.spotify.albumImages[0].url,
-            lyrics: track.lyrics
+            images: track.spotify.albumImages,
+            lyrics: track.lyrics,
+            type: track.type
     }))
 
     const trackDocsStr = JSON.stringify(trackDocs)
-    await fs.writeFile(`./data/elastic/tracks.json`, trackDocsStr)
+    await fs.writeFile(`${docsPath}/tracks.json`, trackDocsStr)
 }
 
-createTrackDocuments()
+const createArtistDocuments = async () => {
+    const artistDocs = artists.map(artist => ({
+        id: artist.id,
+        name: artist.name,
+        genres: artist.genres,
+        styles: artist.styles,
+        images: artist.spotify.images,
+        popularity: artist.spotify.popularity,
+        type: artist.type
+    }))
+
+    const artistDocsStr = JSON.stringify(artistDocs)
+    await fs.writeFile(`${docsPath}/artists.json`, artistDocsStr)
+}
+
+const createAlbumDocuments = async () => {
+    const albumDocs = albums.map(album => ({
+        id: album.id,
+        name: album.name,
+        artistNames: album.artistIds.map(id => artists.find(art => art.id === id).name),
+        genres: album.genres,
+        styles: album.styles,
+        popularity: album.spotify.popularity,
+        totalTracks: album.totalTracks,
+        releaseDate: album.spotify.releaseDate,
+        images: album.spotify.images,
+        type: album.type
+    }))
+
+    const albumsDocsStr = JSON.stringify(albumDocs)
+    await fs.writeFile(`${docsPath}/albums.json`, albumsDocsStr)
+}
+
+const createDocs = async () => {
+    await createTrackDocuments()
+    await createArtistDocuments()
+    await createAlbumDocuments()
+}
+
+createDocs()
--- generate-db/types/elastic.d.ts
+++ generate-db/types/elastic.d.ts
@@ -1,3 +1,9 @@
+interface Image {
+  width: number;
+  height: number;
+  url: string;
+}
+
 interface TrackDocument {
   id: string;
   trackName: string;
@@ -5,8 +11,34 @@ interface TrackDocument {
   albumName: string;
   explict: boolean;
   playCount: number;
+  popularity: number;
+  durationMs: number;
   genres: string[];
   styles: string[];
-  imageUrl: string;
+  images: Image[];
   lyrics: string;
+  type: "track";
+}
+
+interface ArtistDocument {
+  id: string;
+  name: string;
+  genres: string[];
+  styles: string[];
+  images: Image[];
+  popularity: number;
+  type: "artist";
+}
+
+interface AlbumDocument {
+  id: string;
+  name: string;
+  artistNames: string[];
+  genres: string[];
+  styles: string[];
+  popularity: number;
+  totalTracks: number;
+  releaseDate: string;
+  images: Image[];
+  type: "album";
 }
