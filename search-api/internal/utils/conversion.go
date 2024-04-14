4978105dde17553f5d15ae1797b7961cd5990d3b
Nicholas <nick.nogue28@outlook.com>
2024-04-13 22:21:20 -0300

    add utils

---
+++ search-api/internal/utils/conversion.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"encoding/json"
+
+	"github.com/nichol20/rhythmicity/search-api/internal/domain"
+	"github.com/nichol20/rhythmicity/search-api/internal/pb"
+)
+
+func TypeConverter[R any](data any) (*R, error) {
+	var result R
+	b, err := json.Marshal(&data)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(b, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, err
+}
+
+func ImagesToMessage(images []domain.Image) []*pb.Image {
+	var i []*pb.Image
+	for _, v := range images {
+		i = append(i, &pb.Image{
+			Height: v.Height,
+			Width:  v.Width,
+			Url:    v.Url,
+		})
+	}
+	return i
+}
