7060eb83b84d9c8f4cd1dee680b09373cb68507f
Nicholas <nick.nogue28@outlook.com>
2024-04-21 21:27:44 -0300

    add env

--- main-api/internal/db/db.go
+++ main-api/internal/db/db.go
@@ -4,21 +4,24 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func ConnectToDb() *sql.DB {
-	connection := fmt.Sprint(
-		" host=db",
-		" port=5432",
-		" user=postgres",
-		" password=postgres", // unecessary
-		" dbname=postgres",
-		" sslmode=verify-full",
-		" sslrootcert=/app/internal/db/certs/root.crt",
-		" sslkey=/app/internal/db/certs/client.key",
-		" sslcert=/app/internal/db/certs/client.crt",
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := "verify-full"
+	sslrootcert := os.Getenv("DB_ROOT_CERT")
+	sslkey := os.Getenv("DB_SSL_KEY")
+	sslcert := os.Getenv("DB_SSL_CERT")
+
+	connection := fmt.Sprintf(
+		"host=%s port=%s user=%s dbname=%s sslmode=%s sslrootcert=%s sslkey=%s sslcert=%s",
+		host, port, user, dbname, sslmode, sslrootcert, sslkey, sslcert,
 	)
 
 	db, err := sql.Open("postgres", connection)
