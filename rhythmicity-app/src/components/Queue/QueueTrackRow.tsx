a1c7a82432c3824e365ad475069f07c66035c309
Nicholas <nick.nogue28@outlook.com>
2024-05-02 20:49:20 -0300

    add more functionalities

--- rhythmicity-app/src/components/Queue/QueueTrackRow.tsx
+++ rhythmicity-app/src/components/Queue/QueueTrackRow.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { ExplicitSign } from "../ExplicitSign";
 import { deleteIcon, verticalEllipsisIcon } from "@/assets";
 import { useState } from "react";
+import { usePlayback } from "@/contexts/PlaybackContext";
 
 interface QueueTrackRowProps {
     track: Track | SearchedTrack
@@ -14,6 +15,7 @@ interface QueueTrackRowProps {
 export const QueueTrackRow = ({ track }: QueueTrackRowProps) => {
     const [showOptionsBtn, setShowOptionsBtn] = useState(false)
     const [showOptions, setShowOptions] = useState(false)
+    const { skipTo, deleteFromQueue } = usePlayback()
 
     const getImage = (track: SearchedTrack | Track) => {
         if ("spotify" in track) {
@@ -39,11 +41,26 @@ export const QueueTrackRow = ({ track }: QueueTrackRowProps) => {
         setShowOptionsBtn(false)
     }
 
+    const deleteTrack = () => {
+        setShowOptions(false)
+        deleteFromQueue(track.id)
+    }
+
     return (
-        <div key={track.id} className={styles.trackItem} onMouseOver={handleMouseOver} onMouseLeave={handleMoutseLeave}>
+        <div
+            className={styles.trackItem}
+            onMouseOver={handleMouseOver}
+            onMouseLeave={handleMoutseLeave}
+        >
             <div className={styles.info}>
                 <div className={styles.imgBox}>
-                    <Image src={getImage(track)} alt={getName(track)} width={48} height={48} />
+                    <Image
+                        src={getImage(track)}
+                        alt={getName(track)}
+                        width={48}
+                        height={48}
+                        onClick={() => skipTo(track.id)}
+                    />
                 </div>
                 <div className={styles.description}>
                     <span className={styles.name}>{getName(track)}</span>
@@ -61,7 +78,7 @@ export const QueueTrackRow = ({ track }: QueueTrackRowProps) => {
                 )}
                 {showOptions && (
                     <div className={styles.options}>
-                        <button className={styles.optionItem}>
+                        <button className={styles.optionItem} onClick={deleteTrack}>
                             <div className={styles.optoinsImgBox}>
                                 <Image src={deleteIcon} alt="delete" />
                             </div>
