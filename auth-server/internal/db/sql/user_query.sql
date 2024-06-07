375b189970c9083ea4d7da97f9613cc96f7c10a7
Nicholas <nick.nogue28@outlook.com>
2024-06-06 23:18:56 -0300

    add sql

---
+++ auth-server/internal/db/sql/user_query.sql
@@ -0,0 +1,8 @@
+-- name: GetUser :one
+SELECT * FROM users WHERE id = $1;
+
+-- name: GetUserByEmail :one
+SELECT * FROM users WHERE email = $1;
+
+-- name: CreateUser :one
+INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;
