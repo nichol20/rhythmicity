2c7b23b6ef3f35325eb10a061e818e2a06494c23
Nicholas <nick.nogue28@outlook.com>
2024-06-08 22:49:13 -0300

    update sql queries

--- auth-server/internal/db/sql/user_query.sql
+++ auth-server/internal/db/sql/user_query.sql
@@ -6,3 +6,6 @@ SELECT * FROM users WHERE email = $1;
 
 -- name: CreateUser :one
 INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;
+
+-- name: CheckIfEmailExists :one
+SELECT EXISTS(SELECT 1 FROM users WHERE email = $1) AS emailExists;
