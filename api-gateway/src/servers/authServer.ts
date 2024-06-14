ae60e4fc8a12e4525e8459f8016ba74ac03e42e1
Nicholas <nick.nogue28@outlook.com>
2024-06-14 20:10:30 -0300

    add auth server client connection

---
+++ api-gateway/src/servers/authServer.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import * as protoLoader from "@grpc/proto-loader";
+import * as grpcLibrary from "@grpc/grpc-js";
+import { ProtoGrpcType } from "../proto/gen/auth";
+
+const protoDir = path.resolve(__dirname, "../proto/authServer");
+const serverUrl = process.env.AUTH_SERVER_URL || "host.docker.internal:50053";
+
+const protoFile = `${protoDir}/auth.proto`;
+
+const protoLoaderOptions: protoLoader.Options = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+};
+
+const packageDefinition = protoLoader.loadSync(protoFile, protoLoaderOptions);
+const authServerProto = (
+    grpcLibrary.loadPackageDefinition(
+        packageDefinition
+    ) as unknown as ProtoGrpcType
+).rhythmicity.auth_server;
+
+export const authClient = new authServerProto.Auth(
+    serverUrl,
+    grpcLibrary.credentials.createInsecure()
+);
