7b4a6de6fff1fb582f9a370192a6bc5831afefd4
Nicholas <nick.nogue28@outlook.com>
2024-06-27 23:20:48 -0300

    add axios interceptor

--- rhythmicity-app/src/utils/http.ts
+++ rhythmicity-app/src/utils/http.ts
@@ -4,3 +4,14 @@ export const https = axios.create({
     baseURL: 'https://localhost',
     withCredentials: true
 })
+
+https.interceptors.response.use(
+    response => response,
+    async error => {
+        console.log(error)
+        if (error?.response?.status === 403 || error?.response?.status === 401) {
+            window.location.assign("/sign-in")
+        }
+        return Promise.reject(error)
+    }
+)
