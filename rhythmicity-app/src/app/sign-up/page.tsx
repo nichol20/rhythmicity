0fb74a4e5c61252cc7304a841d16732cd1c2ff92
Nicholas <nick.nogue28@outlook.com>
2024-07-03 23:22:45 -0300

    handle sign up

--- rhythmicity-app/src/app/sign-up/page.tsx
+++ rhythmicity-app/src/app/sign-up/page.tsx
@@ -1,25 +1,72 @@
 'use client'
 
 import Link from 'next/link'
-import { redirect } from 'next/navigation'
-import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 import { useAuth } from '@/contexts/AuthContext'
 import { PasswordInput } from '@/components/PasswordInput'
 
 import styles from '@/styles/SignUp.module.scss'
+import { ErrorMessage } from '@/components/ErrorMessage'
 
 export default function SignUpPage() {
-    const { user } = useAuth()
+    const { user, signUp } = useAuth()
+    const router = useRouter()
+    const [emptyFieldError, setEmptyFieldError] = useState(false)
+    const [existingEmailError, setExistingEmailError] = useState(false)
+    const [passwordDoesNotMatchError, setPasswordDoesNotMatchError] = useState(false)
 
-    if (user) {
-        redirect("/")
+    useEffect(() => {
+        if (user) {
+            router.push("/")
+        }
+    }, [user, router])
+
+
+    const resetErrors = () => {
+        setEmptyFieldError(false)
+        setPasswordDoesNotMatchError(false)
+        setExistingEmailError(false)
+    }
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+
+        for (const pair of formData.entries()) {
+            const isFieldEmpty = (pair[1] as string).length === 0
+            if (isFieldEmpty) {
+                setEmptyFieldError(true)
+                return
+            }
+        }
+
+        const username = formData.get("username") as string
+        const email = formData.get('email') as string
+        const password = formData.get('password') as string
+        const confirmPassword = formData.get("confirmPassword") as string
+
+        if (password !== confirmPassword) {
+            return setPasswordDoesNotMatchError(true)
+        }
+
+        try {
+            await signUp(username, email, password)
+            router.push("/")
+        } catch (error: any) {
+            console.log(error)
+            if (error.response?.status === 409) {
+                setExistingEmailError(true)
+                return
+            }
+        }
     }
 
     return (
         <div className={styles.signUpPage}>
             <div className={styles.signUpContainer}>
-                <form className={styles.signUpForm} action="">
+                <form className={styles.signUpForm} onSubmit={handleSubmit}>
                     <div className={styles.field}>
                         <label htmlFor="username">User</label>
                         <input
@@ -28,6 +75,7 @@ export default function SignUpPage() {
                             name='username'
                             spellCheck={false}
                             id='username'
+                            onChange={resetErrors}
                         />
                     </div>
                     <div className={styles.field}>
@@ -38,16 +86,30 @@ export default function SignUpPage() {
                             name='email'
                             spellCheck={false}
                             id='email'
+                            onChange={resetErrors}
                         />
                     </div>
                     <div className={styles.field}>
                         <label htmlFor="password">password</label>
-                        <PasswordInput id="password" name="password" className={styles.fieldInput} />
+                        <PasswordInput
+                            id="password"
+                            name="password"
+                            className={styles.fieldInput}
+                            onChange={resetErrors}
+                        />
                     </div>
                     <div className={styles.field}>
                         <label htmlFor="confirmPassword">confirm password</label>
-                        <PasswordInput id="confirmPassword" name="confirmPassword" className={styles.fieldInput} />
+                        <PasswordInput
+                            id="confirmPassword"
+                            name="confirmPassword"
+                            className={styles.fieldInput}
+                            onChange={resetErrors}
+                        />
                     </div>
+                    {passwordDoesNotMatchError && <ErrorMessage message='the passwords do not match' />}
+                    {emptyFieldError && <ErrorMessage message='you need to fill in all the fields' />}
+                    {existingEmailError && <ErrorMessage message='this email already exists' />}
                     <button type='submit' className={styles.submitBtn}>sign up</button>
                     <span className={styles.registerLinkBox}>
                         {"Already have an account? "}
